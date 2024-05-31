48d959be92cdd1294c85baa1e06d4f482ba0bba4
OwaisSiddiqui <owais.ahmaad@gmail.com>
2024-05-31 02:06:48 -0400

    Update max duration to 60

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -1,10 +1,10 @@
-"use server";
-
 import { Chat } from "@/components/chat";
 import { getSuggestions } from "@/app/(chat)/actions";
 import { cookies } from "next/headers";
 import { auth, signIn as signInAuth } from "@/auth";
 import GuestModal from "@/components/guest-modal";
+
+export const maxDuration = 60;
 
 export default async function HomePage() {
   const session = await auth();
