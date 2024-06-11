10ad8a4215d620d16ee9cc6d7365671edc754d13
OwaisSiddiqui <owais.ahmaad@gmail.com>
2024-06-11 02:19:22 -0400

    Remove guest modal

--- src/app/(chat)/page.tsx
+++ src/app/(chat)/page.tsx
@@ -1,21 +1,12 @@
 import { Chat } from "@/components/chat";
 import { getSuggestions } from "@/app/(chat)/actions";
-import { cookies } from "next/headers";
-import { auth, signIn as signInAuth } from "@/auth";
-import GuestModal from "@/components/guest-modal";
+import { auth } from "@/auth";
 
 export const maxDuration = 60;
 
 export default async function HomePage() {
   const session = await auth();
   const suggestions = await getSuggestions();
-  const cookieStore = cookies();
-  const isGuest = cookieStore.get("isGuest");
-
-  async function signIn(formData: FormData) {
-    "use server";
-    await signInAuth("resend", formData);
-  }
 
   return (
     <>
@@ -24,10 +15,6 @@ export default async function HomePage() {
         session={session}
         id={null}
         suggestions={suggestions}
-      />
-      <GuestModal
-        isGuest={session?.user ? "true" : isGuest?.value ?? ""}
-        signIn={signIn}
       />
     </>
   );
